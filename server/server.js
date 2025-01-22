dc115afcb378e4655fe860e3233869b7e5fac65b
Sanjayjangra7788 <Sanjayjangra7788@gmail.com>
2025-01-23 00:58:39 +0530

    Initial commit for MERN project

--- server/server.js
+++ server/server.js
@@ -74,48 +74,58 @@
 
 
 
-const express = require('express');
-const cors = require('cors');
-const http = require('http');
-const { Server } = require('socket.io');
+// const express = require('express');
+// const cors = require('cors');
+// const http = require('http');
+// const { Server } = require('socket.io');
 
-const app = express();
-const server = http.createServer(app);
-const io = new Server(server, {
-  cors: {
-    origin: "*",  // For development, restrict this for production
-    credentials: true,
-  },
-});
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",  // For development, restrict this for production
+//     credentials: true,
+//   },
+// });
 
-// Sample API Route
-app.get("/api", (req, res) => {
-  res.send("Hello from the backend!");
-});
+// // Sample API Route
+// app.get("/api", (req, res) => {
+//   res.send("Hello from the backend!");
+// });
 
-// Socket.IO connection
-let users = {};
+// // Socket.IO connection
+// let users = {};
 
-io.on('connection', (socket) => {
-  console.log('A new user has connected', socket.id);
+// io.on('connection', (socket) => {
+//   console.log('A new user has connected', socket.id);
 
-  socket.on('join', (username) => {
-    users[socket.id] = username;
-    io.emit('userList', Object.values(users)); // Broadcast the updated user list
-  });
+//   socket.on('join', (username) => {
+//     users[socket.id] = username;
+//     io.emit('userList', Object.values(users)); // Broadcast the updated user list
+//   });
 
-  socket.on('message', (message) => {
-    io.emit('message', message); // Broadcast message to all users
-  });
+//   socket.on('message', (message) => {
+//     io.emit('message', message); // Broadcast message to all users
+//   });
 
-  socket.on('disconnect', () => {
-    delete users[socket.id];
-    io.emit('userList', Object.values(users)); // Broadcast the updated user list
-  });
-});
+//   socket.on('disconnect', () => {
+//     delete users[socket.id];
+//     io.emit('userList', Object.values(users)); // Broadcast the updated user list
+//   });
+// });
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// const PORT = process.env.PORT || 5000;
+// server.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+const path = require("path");
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
 });
 
