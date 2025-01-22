56dab68abe1406b24ba179e05351fb808f4edd00
Sanjayjangra7788 <Sanjayjangra7788@gmail.com>
2025-01-22 14:32:12 +0530

    Initial commit for MERN project

--- server/server.js
+++ server/server.js
@@ -35,6 +35,45 @@
 
 
 // server.js
+// const express = require('express');
+// const http = require('http');
+// const { Server } = require('socket.io');
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = new Server(server, {
+//   cors: {
+//     origin: "*",
+//     credentials: true,
+//   },
+// });
+
+// let users = {};
+
+// io.on('connection', (socket) => {
+//   console.log('A new user has connected', socket.id);
+
+//   socket.on('join', (username) => {
+//     users[socket.id] = username;
+//     io.emit('userList', Object.values(users)); // Broadcast the updated user list
+//   });
+
+//   socket.on('message', (message) => {
+//     io.emit('message', message); // Broadcast message to all users
+//   });
+
+//   socket.on('disconnect', () => {
+//     delete users[socket.id];
+//     io.emit('userList', Object.values(users)); // Broadcast the updated user list
+//   });
+// });
+
+// server.listen(5000, () => {
+//   console.log('Server is running on port 5000');
+// });
+
+
+
 const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
@@ -43,7 +82,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "*",
+    origin: "*", // Allows all origins. Change this to a more restrictive URL in production.
     credentials: true,
   },
 });
@@ -71,3 +110,4 @@ io.on('connection', (socket) => {
 server.listen(5000, () => {
   console.log('Server is running on port 5000');
 });
+
